133cc2df47beb02e67d541341c5c6ae8f87726d2
yaj-23 <yajurva.trivedi@ryerson.ca>
2022-02-16 09:28:27 -0500

    changes

--- lib/dash/screens/Game_OnePage.dart
+++ lib/dash/screens/Game_OnePage.dart
@@ -2,20 +2,24 @@ import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
 class Game_OnePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.pinkAccent,
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
             Container(
               alignment: Alignment.topLeft,
-              padding: EdgeInsets.fromLTRB(10, 50, 0, 0,),
+              padding: EdgeInsets.fromLTRB(
+                15,
+                50,
+                0,
+                0,
+              ),
               child: Text(
-                'Game1',
+                'emotional',
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 18,
@@ -25,17 +29,52 @@ class Game_OnePage extends StatelessWidget {
             ),
             Container(
               alignment: Alignment.topLeft,
-              padding: EdgeInsets.fromLTRB(10, 0, 0, 0,),
-              child: Text(
-                'tester1',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontFamily: 'SF Display',
-                  fontWeight: FontWeight.w100
-                ),
+              padding: EdgeInsets.fromLTRB(
+                15,
+                0,
+                0,
+                0,
               ),
-            )
+              child: Text(
+                'intelligence lab',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: 'SF Display',
+                    fontWeight: FontWeight.w100),
+              ),
+            ),
+            SizedBox(height: 30),
+            Stack(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(50, 0, 0, 0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "emotion",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 58, 56, 56),
+                          fontSize: 75,
+                          fontFamily: 'SF Display',
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 70, 0, 0),
+                  child: Align(
+                      child: Text(
+                    "recognition training",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'SF Display',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 40),
+                  )),
+                ),
+              ],
+            ),
           ],
         ),
       ),
